e639c972c4e66cdf26ce487aaab921ef467172b4
networkserver06-commits <networkserver06-commits@users.noreply.github.com>
2026-09-12 10:07:11 +0000

    Switch payments from CourtneyTech to LeeTec

--- server/env.ts
+++ server/env.ts
@@ -3,11 +3,10 @@ export const ENV = {
   mongodbDatabase: process.env.MONGODB_DATABASE ?? "leetec_vpn_bot",
   telegramBotToken: process.env.TELEGRAM_BOT_TOKEN ?? "",
   telegramAdminUserId: process.env.TELEGRAM_ADMIN_USER_ID ?? "",
-  courtneyBaseUrl: process.env.COURTNEY_BASE_URL ?? "https://courtneytech.xyz/api",
-  courtneyApiKey: process.env.COURTNEY_API_KEY ?? "",
-  courtneyApiSecret: process.env.COURTNEY_API_SECRET ?? "",
-  courtneyAccountId: Number(process.env.COURTNEY_ACCOUNT_ID ?? 0),
+  leetecApiKey: process.env.LEETEC_API_KEY ?? "",
+  leetecTillId: process.env.LEETEC_TILL_ID ? Number(process.env.LEETEC_TILL_ID) : undefined,
+  leetecStatusUrl: process.env.LEETEC_STATUS_URL ?? "",
   publicAppUrl: (process.env.PUBLIC_APP_URL ?? "").replace(/\/+$/, ""),
   telegramBotUsername: process.env.TELEGRAM_BOT_USERNAME ?? "LeeTechadmin_Bot",
 };
-export function missingEnv() { return [["MONGODB_URI", ENV.mongodbUri], ["TELEGRAM_BOT_TOKEN", ENV.telegramBotToken], ["TELEGRAM_ADMIN_USER_ID", ENV.telegramAdminUserId], ["COURTNEY_API_KEY", ENV.courtneyApiKey], ["COURTNEY_API_SECRET", ENV.courtneyApiSecret], ["COURTNEY_ACCOUNT_ID", ENV.courtneyAccountId], ["PUBLIC_APP_URL", ENV.publicAppUrl]].filter(([, value]) => !value).map(([name]) => name); }
+export function missingEnv() { return [["MONGODB_URI", ENV.mongodbUri], ["TELEGRAM_BOT_TOKEN", ENV.telegramBotToken], ["TELEGRAM_ADMIN_USER_ID", ENV.telegramAdminUserId], ["LEETEC_API_KEY", ENV.leetecApiKey], ["PUBLIC_APP_URL", ENV.publicAppUrl]].filter(([, value]) => !value).map(([name]) => name); }
